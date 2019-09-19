bc3089072ee0b35b741a59c23e8b9c39e49a7974
chason <chasonlsmith@gmail.com>
2019-09-18 22:17:47 -0600

    Began adding arrays for Q/A also started function to begin the game. Still working on using my button to start the whole thing.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,23 +7,27 @@ var timer = 25;
 var correct = 0;
 var incorrect = 0;
 
+
+//Still need to create functions to start the game on button click.
+//Need to create code for 
+
 // question and answer array
 var questions = [
     {// question 1
-        question: "What is the name of the ship in the picture?",
-        choices: [" "],
-        correctA: " ",
+        question: "What is the name of the ship in the picture? ",
+        choices: ["The Slaver", "Slave I", "Millennium Falcon", "X-Wing"],
+        correctA: "Slave I",
         image: " "
     },
     {//question 2
-        question: " ",
-        choices: [" "],
+        question: "Who was the leader of the Empire? ",
+        choices: ["Darth Vader", "Leia Organa", "Grand Moff Tarkin", "Sheev Palpatine"],
         correctA: " ",
         image: " "
     },
     {//question 3
-        question: " ",
-        choices: [" "],
+        question: "Which movie was the second Star Wars film? ",
+        choices: ["A New Hope", "A Phantom Menace", "Empire Strikes Back", "Rise of Skywalker"],
         correctA: " ",
         image: " "
     },
@@ -70,4 +74,27 @@ var questions = [
         image: " "
     },
 ]
-})
+});
+
+//creating questions and question count
+
+function questionContent() {
+    $("#gameUi").append("<p><strong>" +
+        questions[questionsCount].question +
+        //Confusing with double quotes on different elements, classes, and variables using single quotes.
+        "</p><p class='choices'>" +
+        questions[questionCount].choices[0] +
+        "</p><p class='choices'>" + 
+        questions[questionCount].choices[1] + 
+        "</p><p class='choices'>" + 
+        questions[questionCount].choices[2] +
+        "</p><p class='choices'>" + 
+        questions[questionCount].choices[3] + 
+        "</strong></p>");
+}
+//<div class="jumbotron text-center" id="gameUi">
+// This is to start the game using p-tags and how much time you have at start
+function startGame() {
+    $("#gameUi").html("<p>You have <span id='time'>" + time + "</span> Second left!</p>");
+    $("#start")    
+}
