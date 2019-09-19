2ff35d7ab8e32362fffb7ef4452265ebe345f1f7
chason <chasonlsmith@gmail.com>
2019-09-18 21:22:22 -0600

    added var array for questions, answers, added background image. Attempted to upload a cursor icon but couldn't get it to work. Adjusted spacing and still need to make changes to the margins and padding for my div.

---
+++ assets/javascript/app.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+
+// variables for initial time, and question count
+var questionCount = 0;
+var timer = 25;
+// End game results and counters right and wrong
+var correct = 0;
+var incorrect = 0;
+
+// question and answer array
+var questions = [
+    {// question 1
+        question: "What is the name of the ship in the picture?",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+    {//question 2
+        question: " ",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+    {//question 3
+        question: " ",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+    {//question 4
+        question: " ",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+    {//question 5
+        question: " ",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+    {//question 6
+        question: " ",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+    {//question 7
+        question: " ",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+    {//question 8
+        question: " ",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+    {//question 9
+        question: " ",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+    {//question 10
+        question: " ",
+        choices: [" "],
+        correctA: " ",
+        image: " "
+    },
+]
+})
