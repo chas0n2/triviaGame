a2a2f4de7248668b12b06755022c4b7f6e6b4159
chason <chasonlsmith@gmail.com>
2020-01-30 23:32:19 -0700

    updated js file added more questions and set global variables.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,54 +1,125 @@
     $(document).ready(function () {
         // $(‘#’).hide();
        
-// variables for initial time, and question count
-var questionCount = 0;
-var timer = 25;
-// End game results and counters right and wrong
-var correct = 0;
-var incorrect = 0;
+// // variables for initial time, and question count
+// var questionCount = 0;
+// var timer = 25;
+// // End game results and counters right and wrong
+// var correct = 0;
+// var incorrect = 0;
 
 
 //Still need to create functions to start the game on button click.
 // I would like the game to display the list of questions then give you a timer to choose the correct answer.
 
 
-
+//creating questions and question count
 // question and answer array
-var questions = [
+let questions = [
     {// question 1
-        questions: "What is the name of the ship in the picture?",
-        choices: ["The Slaver", "Slave I", "Millennium Falcon", "X-Wing"],
+        questions: "What is the name of the Boba Fett's Ship?",
+        C0: "<h4><button class='btn'>The Slaver</button></h4>",
+        C1: "<h4><button class='btn correct'>Slave I</button></h4>",
+        C2: "<h4><button class='btn'>Millenium Falcon</button></h4>",
+        C3: "<h4><button class='btn'>X-Wing</button></h4>",
         correctA: "Slave I"
         
     },
     {//question 2
         questions: "Who was the leader of the Empire?",
-        choices: ["Darth Vader", "Leia Organa", "Grand Moff Tarkin", "Sheev Palpatine"],
-        correctA: "Sheev Palpatine",
+        C0: "<h4><button class='btn'>Darth Vader</button></h4>",
+        C1: "<h4><button class='btn'>Leia Organa</button></h4>",
+        C2: "<h4><button class='btn'>Grand Moff Tarkin</button></h4>",
+        C3: "<h4><button class='btn correct'>XSheev Palpatine</button></h4>",
+        correctA: "Sheev Palpatine"
         
     },
     {//question 3
         questions: "Which was the second Star Wars film?",
-        choices: ["A New Hope", "A Phantom Menace", "Empire Strikes Back", "Rise of Skywalker"],
-        correctA: "Empire Strikes Back",
-        
+        C0: "<h4><button class='btn'>A New Hope</button></h4>",
+        C1: "<h4><button class='btn'>A Phantom Menace</button></h4>",
+        C2: "<h4><button class='btn correct'>Empire Strikes Back</button></h4>",
+        C3: "<h4><button class='btn'>Rise of Skywalker</button></h4>",
+        correctA: "Empire Strikes Back"
     },
     {//question 4
         questions: "Which order brought about the death of the Jedi?",
-        choices: ["Order 55", "Order 66", "Order 33", "Order a burger"],
-        correctA: "Order 66",
-        
+        C0: "<h4><button class='btn'>1</button></h4>",
+        C1: "<h4><button class='btn'>10</button></h4>",
+        C2: "<h4><button class='btn correct'>2</button></h4>",
+        C3: "<h4><button class='btn'>6</button></h4>",
+        correctA: "2"
     },
-    ]
-    
-    $("#Start").on("click", function() {
-        for (i = 0; i < questions.length; i++) {
-           var questionHtml = buildQuestion(questions[i])
-            $("#gameUi").append(questionHtml)
-          }
-          $("#Start").hide();
-    }) 
+    {//question 5
+        questions: "How many suns does Tatoonie have?",
+        C0: "<h4><button class='btn'>Order 55</button></h4>",
+        C1: "<h4><button class='btn correct'>Order 66</button></h4>",
+        C2: "<h4><button class='btn'>Order 33</button></h4>",
+        C3: "<h4><button class='btn'>Order a burger</button></h4>",
+        correctA: "Order 66"
+    },
+    {//question 6
+        questions: "What species is Jabba?",
+        C0: "<h4><button class='btn'>Ithorian</button></h4>",
+        C1: "<h4><button class='btn'>Jawa</button></h4>",
+        C2: "<h4><button class='btn'>Jenet</button></h4>",
+        C3: "<h4><button class='btn correct'>Hutt</button></h4>",
+        correctA: "Hutt"
+    },
+    {//question 7
+        questions: "Who played Princess Leia?",
+        C0: "<h4><button class='btn'>Gillian Anderson</button></h4>",
+        C1: "<h4><button class='btn'>Sigourney Weaver</button></h4>",
+        C2: "<h4><button class='btn'>Linda Hamilton</button></h4>",
+        C3: "<h4><button class='btn correct'>Carrie Fisher</button></h4>",
+        correctA: "Carrie Fisher"
+    },
+    {//question 8
+        questions: "Who are the only two characters who appear in every Star Wars movie?",
+        C0: "<h4><button class='btn correct'>C-3PO and R2-D2</button></h4>",
+        C1: "<h4><button class='btn'>Luke and Leia</button></h4>",
+        C2: "<h4><button class='btn'>Han and Chewbacca</button></h4>",
+        C3: "<h4><button class='btn'>Darth Vader and Emperor Palpatine</button></h4>",
+        correctA: "C-3PO and R2-D2"
+    },
+    {//question 9
+        questions: "On which planet do we first meet Rey in The Force Awakens?",
+        C0: "<h4><button class='btn'>Farlax</button></h4>",
+        C1: "<h4><button class='btn'>Tatoonie</button></h4>",
+        C2: "<h4><button class='btn'>Dantooine</button></h4>",
+        C3: "<h4><button class='btn correct'>Jakku</button></h4>",
+        correctA: "Jakku"
+    },
+    {//question 10
+        questions: "Who is Boba Fett’s father?",
+        C0: "<h4><button class='btn'>Bongo Fett</button></h4>",
+        C1: "<h4><button class='btn'>Dango Fett</button></h4>",
+        C2: "<h4><button class='btn correct'>Jango Fett</button></h4>",
+        C3: "<h4><button class='btn'>Django Fett</button></h4>",
+        correctA: "Jango Fett"
+    }
+    ];
+
+
+    //global variables
+    let gameOver;
+    let currentIndex;
+    let correct;
+    let incorrect;
+    let alreadyAnswered;
+    let startSong;
+    let seconds;
+    let intervalId;
+
+
+    //start the game on button click.
+    // $("#Start").on("click", function() {
+    //     for (i = 0; i < questions.length; i++) {
+    //        var questionHtml = buildQuestion(questions[i])
+    //         $("#gameUi").append(questionHtml)
+    //       }// hide start after onclick
+    //       $("#Start").hide();
+    // }) 
     
     function buildQuestion(question) {
         var questionContainer = $("<div></div>")
@@ -66,7 +137,7 @@ var questions = [
 
 });
 
-//creating questions and question count
+
 
 
  
